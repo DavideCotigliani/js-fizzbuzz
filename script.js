2a4e74fb9a855dc7da245e209512a7a1332f72e5
DavideCotigliani <davide.cotigliani@gmail.com>
2025-03-18 15:38:08 +0100

    [UPD] Updated the structure for with multiples of 3 and 5

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //Definisco le variabili
 
-
 for(let i=1;i<=100;i++){
-    if(i % 2 === 0){
-        console.log(`${i} è pari`);    
+    if(i % 3 === 0){
+        console.log(`Fizz`);    
+    }
+    else if (i % 5 === 0){
+        console.log(`Buzz`)
     }
     else{
-        console.log(`${i} è dispari`)
+        console.log(`${i}`)
     }
     
 }
